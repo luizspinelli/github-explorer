ce5f1c91bc97c0625e5b1f4602e4a9fd4a080be2
Luiz Spinelli <luiz.luchi@gmail.com>
2021-03-22 23:29:08 -0300

    fist component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,18 @@
+export function RepositoryList() {
+  return (
+    <section>
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
