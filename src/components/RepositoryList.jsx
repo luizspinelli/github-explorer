4a56f78f2d23bf1e24733eea7b74118c7307bd55
Luiz Spinelli <luiz.luchi@gmail.com>
2021-03-23 20:26:11 -0300

    consuming github api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
+import { useEffect, useState } from "react";
+import "../styles/repositories.scss";
 import { RepositoryItem } from "./RepositoryItem";
 
-import "../styles/repositories.scss";
 
 const repository = {
   name: "unform2",
@@ -9,6 +10,14 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
